29077e14d700d8ba4ee28dddb158fcc971865f56
Marta Górska <magorska.95@gmail.com>
2021-07-05 10:50:57 +0200

    28.4 endpoint

---
+++ src/test/java/com/crud/tasks/controller/TrelloControllerTest.java
@@ -0,0 +1,57 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoard;
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloList;
+import com.crud.tasks.domain.TrelloListDto;
+import com.crud.tasks.trello.client.facade.TrelloFacade;
+import org.apache.catalina.LifecycleState;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.web.SpringJUnitWebConfig;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultMatcher;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.client.match.MockRestRequestMatchers.jsonPath;
+
+@SpringJUnitWebConfig
+@WebMvcTest(TrelloController.class)
+class TrelloControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private TrelloFacade trelloFacade;
+
+    @Test
+    void shouldGetTrelloBoards() throws Exception {
+        //Given
+        TrelloListDto trelloListDto = new TrelloListDto("#1", "testName", false);
+        List<TrelloListDto> trelloLists = Arrays.asList(trelloListDto);
+
+        TrelloBoardDto trelloBoardDto = new TrelloBoardDto("#01", "testName", trelloLists);
+        List<TrelloBoardDto> trelloBoards = Arrays.asList(trelloBoardDto);
+
+        when(trelloFacade.fetchTrelloBoards()).thenReturn(trelloBoards);
+
+        //When&Then
+        mockMvc
+                .perform(MockMvcRequestBuilders
+                .get("/v1/trello/getTrelloBoards"))
+                .andExpect(MockMvcResultMatchers.status().is(200))
+                .andExpect((ResultMatcher) jsonPath("$", hasSize(1)))
+                .andExpect((ResultMatcher) jsonPath("$[0].lists", hasSize(1)));
+    }
+
+}
---
+++ src/test/java/com/crud/tasks/scheduler/EmailSchedulerTest.java
@@ -0,0 +1,49 @@
+package com.crud.tasks.scheduler;
+
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.domain.Mail;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.repository.TaskRepository;
+import com.crud.tasks.service.SimpleEmailService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+@ExtendWith(MockitoExtension.class)
+class EmailSchedulerTest {
+
+    @InjectMocks
+    private EmailScheduler emailScheduler;
+
+    @Mock
+    private TaskRepository taskRepository;
+
+    @Mock
+    private SimpleEmailService simpleEmailService;
+
+    @Mock
+    private AdminConfig adminConfig;
+
+    @Test
+    void testSendInformationEmail() {
+
+        //Given
+        Mail mail = new Mail("test@test.com", "Test", "Test Message", null);
+        when(adminConfig.getAdminMail()).thenReturn(mail.getMailTo());
+        when(taskRepository.count()).thenReturn(1L);
+
+        //When
+        emailScheduler.sendInformationEmail();
+
+        //Then
+        verify(simpleEmailService, times(1)).send(mail);
+
+
+    }
+
+}
