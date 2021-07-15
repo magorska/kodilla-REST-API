bc871d23cbc27f1c85d3e71454b32cfbcb68e4bc
Marta Górska <magorska.95@gmail.com>
2021-07-15 12:59:56 +0200

    31.2 MailCreatorService

---
+++ src/main/java/com/crud/tasks/config/CompanyConfig.java
@@ -0,0 +1,19 @@
+package com.crud.tasks.config;
+
+import lombok.Getter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Getter
+public class CompanyConfig {
+
+    @Value("${info.company.name}")
+    private String infoCompanyName;
+
+    @Value("${info.company.email}")
+    private String infoCompanyEmail;
+
+    @Value("${info.company.phone}")
+    private int infoCompanyPhone;
+}
--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,5 +1,7 @@
 package com.crud.tasks.service;
 
+import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -11,12 +13,25 @@ import org.thymeleaf.context.Context;
 public class MailCreatorService {
 
     @Autowired
+    private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
+
+    @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
 
     public String buildTrelloCardEmail(String message) {
         Context context = new Context();
         context.setVariable("message", message);
+        context.setVariable("tasks_url", "http://localhost:8888/crud");
+        context.setVariable("button", "Visit website");
+        context.setVariable("admin_name", adminConfig.getAdminName());
+        context.setVariable("preview_message", "Trello card has benn created");
+        context.setVariable("goodbye_message", "Have a nice day.");
+        context.setVariable("company_details", companyConfig.getInfoCompanyName() + "\n"
+                + companyConfig.getInfoCompanyEmail() + "\n" + companyConfig.getInfoCompanyPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
