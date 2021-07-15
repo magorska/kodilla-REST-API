3cef7517228792b9c3cfe266673ab34240f92974
Marta Górska <magorska.95@gmail.com>
2021-07-15 12:03:42 +0200

    31.1. Thymleaf

--- src/main/java/com/crud/tasks/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasks/controller/StaticWebPageController.java
@@ -3,11 +3,25 @@ package com.crud.tasks.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class StaticWebPageController {
 
+//    @RequestMapping("/")
+//    public String index(Map<String, String> model) {
+//        model.put("variable", "My Thymeleaf variable");
+//        return "index";
+//    }
+
     @RequestMapping("/")
-    public String index() {
+    public String index(Map<String, Object> model) {
+        model.put("variable", "My Thymeleaf variable");
+        model.put("one", 1);
+        model.put("two", 2);
+        model.put("multiply_operation", "2 * 2 = ");
+        model.put("multi_add_operation", "2 * 2 + 2 = ");
+        model.put("sub_multi_operation", "2 - (2 * 2) = ");
         return "index";
     }
 }
